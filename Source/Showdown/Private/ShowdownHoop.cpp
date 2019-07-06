b467b1f3453d42fd7aa8299dc2fbe7299a6a0940
MaesterLazer <daniel.l.lazar@gmail.com>
2019-07-06 12:57:31 -0700

    ref pointer cleanup, char controller basic self/ball variables, ball pickup fixed, shooting socket added

---
+++ Source/Showdown/Private/ShowdownHoop.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShowdownHoop.h"
+
+// Sets default values
+AShowdownHoop::AShowdownHoop()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShowdownHoop::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShowdownHoop::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
