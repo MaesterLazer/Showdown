d2b8732e693e6afa361d344de5997ebf8a80be25
Simon Norman <zhymon.norman@gmail.com>
2021-01-16 14:54:55 -0500

    remove moment js

--- UI/gameplay/js/main.js
+++ UI/gameplay/js/main.js
@@ -17,6 +17,22 @@
 
 //#endregion BINDING
 
+function format(time) {   
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
 //#region WEARDOWNBAR
 function tickBar(){
     setInterval(()=>{
@@ -155,9 +171,8 @@ tickBar();
     }
 
     function updateGameClock(value){
-        let formatted = moment.utc(value*1000).format('mm:ss');
-
-        bindings.gameClock = formatted;
+        let str = formatted(value)
+        bindings.gameClock = str;
     }
 
     function clearMessage(){
