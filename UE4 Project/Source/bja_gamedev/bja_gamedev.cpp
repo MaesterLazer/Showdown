7f128f5d6db57a881be76c2ad0e843458755afd8
Simon Norman <zhymon.norman@gmail.com>
2021-05-07 09:43:16 -0400

    eod commit

--- UE4 Project/Source/bja_gamedev/bja_gamedev.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "bja_gamedev.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, bja_gamedev, "bja_gamedev" );
--- UE4 Project/Source/bja_gamedev/bja_gamedevGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "bja_gamedevGameModeBase.h"
-
